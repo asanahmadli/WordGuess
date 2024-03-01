3b544d8ef47e2ec4e4fcb8f74c408b414f6fe8c2
asanahmadli <asenahmedli@gmail.com>
2024-03-01 09:41:19 -0500

    I added randum number generator

--- src/main/java/com/github/zipcodewilmington/sample/Person.java
+++ src/main/java/com/github/zipcodewilmington/sample/Person.java
@@ -1,7 +1,40 @@
 package com.github.zipcodewilmington.sample;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/** Asan Ahmadli */
 public class Person {
-    /** Asan Ahmadli */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Let's Play Word guess version 1.0");
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("have");
+        words.add("car");
+        words.add("apple");
+        words.add("banana");
+
+        //create a random numbers
+        Random random = new Random();
+        int number = random.nextInt(4);
+        String word = words.get(number);
+        char[] wordArray = word.toCharArray();
+
+        for(int i=0;i<wordArray.length;i++){
+            System.out.println("Current Guesses:");
+            System.out.println("Enter a single character: ");
+            char character = scan.next().charAt(0);
+            if(wordArray[i]==character){
+                System.out.println(wordArray);
+            }
+
+        }
+
+
+    }
 
 
 
